b90865eddd51c4a366ed684a7ba0416711b77a55
Hutomo Rahadiant <hrahadiant@gmail.com>
2020-03-25 00:44:08 +0700

    maximumSubArrays.js + simple solution for mergeSortedArrays

---
+++ maximumSubArrays.js
@@ -0,0 +1,44 @@
+const sumOfRange = (lower, upper, arr) => {
+    let sum = 0;
+    for (let i=lower; i<=upper; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+
+const highValInArray = arr => {
+    let temp = 0
+    for (let i=0; i<arr.length; i++) {
+        if (temp < arr[i]) {
+            temp = arr[i]
+        }
+    }
+    return temp
+}
+
+const maximumSubArrays = nums => {
+    if (Array.isArray(nums) && nums.length > 0) {
+        let end = nums.length - 1
+        let start = 0
+        let totalInArray = []
+
+        while(start <= end) {
+            partialSum = sumOfRange(start, end, nums)
+            start++
+            if (start == end) {
+                start = 0
+                end--
+            }
+            totalInArray.push(partialSum)
+        }
+
+        return highValInArray(totalInArray)
+    }
+    return "ERROR: Invalid type of argument. Your input is " + typeof(nums)
+}
+
+var array1 = [-2,1,-3,4,-1,2,1,-5,4]
+
+var res = maximumSubArrays(array1)
+console.log(res)
+
--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -4,7 +4,7 @@ example : arg1 = [1, 4, 5, 10]; arg2 = [0, 2, 11]
 expected : [0, 1, 2, 4, 5, 10, 11]
 */
 const mergeSortedArrays = (arr1, arr2) => {
-  if (Array.isArray(arr1) && Array.isArray(arr2)) {
+  if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length > 0 && arr2.length > 0) {
     var sorted = []
     var index = 0
     const totalItems = array1.length + array2.length
@@ -19,5 +19,21 @@ const mergeSortedArrays = (arr1, arr2) => {
     }
     return sorted
   }
-  return "ERROR: Invalid type of argument. Your input is " + typeof(arr1) + " and " + typeof(arr2)
+  return "ERROR: Invalid type of argument or given array is less than 1."
+}
+
+const merging = (arr1, arr2) => {
+  for (let i=0; i<arr2.length; i++) {
+    arr1.push(arr2[i])
+  }
+  return arr1
+}
+
+// simple line of code with sort: O(1)
+const mergeSortedArrays1 = (arr1, arr2) => {
+  if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length > 0 && arr2.length > 0) {
+    merge = merging(arr1, arr2)
+    return merge.sort((low, high) => { return low-high })
+  }
+  return "ERROR: Invalid type of argument or given array is less than 1."
 }
